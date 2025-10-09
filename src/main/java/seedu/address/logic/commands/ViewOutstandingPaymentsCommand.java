ba45ae0eee70ca392894c23d9a1b30221c9724e9
samthambad <98635284+samthambad@users.noreply.github.com>
2025-10-10 05:02:06 +0800

    Intial Command file

--- src/main/java/seedu/address/logic/commands/ViewOutstandingPaymentsCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewOutstandingPaymentsCommand.java
@@ -1,8 +1,24 @@
 package seedu.address.logic.commands;
 
-public class ViewOutstandingPaymentsCommand {
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.finance.Finance;
+import seedu.address.model.Model;
+import java.util.List;
+import static java.util.Objects.requireNonNull;
+
+public class ViewOutstandingPaymentsCommand extends Command {
     public static final String COMMAND_WORD = "outstanding";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": View all outstanding payments.\n"
             + "Example: " + COMMAND_WORD;
+    
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Finance> outstandingPaymentsList = model.getOutstandingPaymentsList();
+        if (outstandingPaymentsList.isEmpty()) {
+            return new CommandResult("No outstanding payments found.");
+        }
+        return new CommandResult("Listed " + outstandingPaymentsList.size() + " outstanding payments.");
+    }
 }
