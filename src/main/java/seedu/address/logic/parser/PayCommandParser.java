5d08993ad16e9fca2d497412a54630772bdd2e6d
samthambad <98635284+samthambad@users.noreply.github.com>
2025-10-13 21:47:49 +0800

    Complete parser

--- src/main/java/seedu/address/logic/parser/PayCommandParser.java
+++ src/main/java/seedu/address/logic/parser/PayCommandParser.java
@@ -2,9 +2,12 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_AMOUNT;
 
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.PayCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.finance.FinanceAmount;
 
 public class PayCommandParser implements Parser<PayCommand> {
 
@@ -14,6 +17,20 @@ public class PayCommandParser implements Parser<PayCommand> {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                     PayCommand.MESSAGE_USAGE));
         }
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_AMOUNT);
 
+        Index index;
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, PayCommand.MESSAGE_USAGE), pe);
+        }
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_AMOUNT);
+
+        if (argMultimap.getValue(PREFIX_AMOUNT).isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, PayCommand.MESSAGE_USAGE));
+        }
+        FinanceAmount payment = ParserUtil.parseFinanceAmount(argMultimap.getValue(PREFIX_AMOUNT).get());
+        return new PayCommand(index, payment);
     }
 }
