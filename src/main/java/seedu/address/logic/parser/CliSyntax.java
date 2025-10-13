0a38f1f5984768007bf3d407c8ba8ab711101ee5
samthambad <98635284+samthambad@users.noreply.github.com>
2025-10-13 20:36:49 +0800

    Differentiate tag names

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -9,8 +9,8 @@ public class CliSyntax {
     public static final Prefix PREFIX_NAME = new Prefix("n/");
     public static final Prefix PREFIX_PHONE = new Prefix("p/");
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
-    public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
-    public static final Prefix PREFIX_TAG = new Prefix("t/");
+    public static final Prefix PREFIX_ADDRESS = new Prefix("addr/");
+    public static final Prefix PREFIX_TAG = new Prefix("tag/");
     public static final Prefix PREFIX_AMOUNT = new Prefix("amt/");
     public static final Prefix PREFIX_FINANCE_TYPE = new Prefix("ftype/");
     public static final Prefix PREFIX_FINANCE_STATUS = new Prefix("fstatus/");
