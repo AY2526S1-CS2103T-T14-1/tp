1d90eedbaee98774b12d547c5bc07311e83597cd
samthambad <98635284+samthambad@users.noreply.github.com>
2025-10-13 20:31:19 +0800

    First commit

---
+++ src/main/java/seedu/address/logic/commands/PayCommand.java
@@ -0,0 +1,13 @@
+package main.java.seedu.address.logic.commands;
+
+
+public class PayCommand {
+
+    public static final String COMMAND_WORD = "pay";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Indicate as student paid.\n"
+            + "Parameters: "
+            + "Example: " + COMMAND_WORD + " "
+            + "INDEX " + ;
+    
+}
