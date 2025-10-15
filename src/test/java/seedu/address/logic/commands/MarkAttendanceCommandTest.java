238054fe1a49baea97ab22cbb8938ea2fa0bf0db
Jaey0un <jeonjaeyoun4@gmail.com>
2025-10-16 00:37:05 +0800

    Add tests for MarkAttendanceCommand and Parser

---
+++ src/test/java/seedu/address/logic/commands/MarkAttendanceCommandTest.java
@@ -0,0 +1,49 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.lesson.Attendance;
+import seedu.address.model.person.Person;
+import seedu.address.testutil.PersonBuilder;
+
+public class MarkAttendanceCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void execute_validIndexAndStatusUnfilteredList_success() {
+        // Create a new person with a lesson for testing purposes.
+        Person firstPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        Person personWithLesson = new PersonBuilder(firstPerson).withLesson("Math", "Monday", "10:00", "Room1").build();
+        model.setPerson(firstPerson, personWithLesson);
+
+        MarkAttendanceCommand markAttendanceCommand = new MarkAttendanceCommand(INDEX_FIRST_PERSON, new Attendance("present"));
+
+        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        Person expectedPerson = new PersonBuilder(personWithLesson).build().markAttendance("present");
+        expectedModel.setPerson(personWithLesson, expectedPerson);
+        String expectedMessage = String.format(MarkAttendanceCommand.MESSAGE_MARK_ATTENDANCE_SUCCESS,
+                expectedPerson.getName(), new Attendance("present"));
+
+        assertCommandSuccess(markAttendanceCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_personWithoutLesson_throwsCommandException() {
+        MarkAttendanceCommand markAttendanceCommand = new MarkAttendanceCommand(INDEX_SECOND_PERSON, new Attendance("present"));
+        Person personWithoutLesson = model.getFilteredPersonList().get(INDEX_SECOND_PERSON.getZeroBased());
+        String expectedMessage = String.format(MarkAttendanceCommand.MESSAGE_PERSON_HAS_NO_LESSON,
+                personWithoutLesson.getName());
+
+        assertCommandFailure(markAttendanceCommand, model, expectedMessage);
+    }
+}
