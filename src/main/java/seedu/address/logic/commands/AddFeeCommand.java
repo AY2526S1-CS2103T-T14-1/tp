87727e01f336b41e6141c660e29bdbf6c7c0773d
Jaey0un <jeonjaeyoun4@gmail.com>
2025-10-30 02:33:47 +0800

    Fix Checkstyle

--- src/main/java/seedu/address/logic/commands/AddFeeCommand.java
+++ src/main/java/seedu/address/logic/commands/AddFeeCommand.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Optional;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.finance.Finance;
